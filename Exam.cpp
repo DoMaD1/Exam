1ed95e8a4ea0dd1983de86368b7a0235784f2564
DoMaD1 <152097259+DoMaD1@users.noreply.github.com>
2023-12-03 13:31:51 +0200

    Add files via upload

---
+++ Exam.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+int res (int a, int b, int c){
+	int r = b / a;
+	if (c <= r) {
+		return c;
+	}
+	else {
+		return r;
+	}
+
+}
+int main()
+{
+	setlocale(0, ".1251");
+	int a, b, c;
+	cout << "Введiть цiну однiєi пляшки: ";
+	cin >> a;
+	cout << "Введiть кiлькiсть коштiв у хлопчика: ";
+	cin >> b;
+	cout << "Введiть кiлькiсть пляшок напою в автоматi: ";
+	cin >> c;
+	cout << res(a, b, c);
+	return 0;
+}
